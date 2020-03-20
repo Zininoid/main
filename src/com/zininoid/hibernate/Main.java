f293f2911c14a44e8cf4f43ec0f8f7f7964824cb
Zinovkin Sergey <zinovkin.s.alek@sberbank.ru>
2020-03-20 17:19:20 +0300

    Some changes

--- src/com/zininoid/hibernate/Main.java
+++ src/com/zininoid/hibernate/Main.java
@@ -8,15 +8,7 @@ public class Main
 	public static void main(String[] args)
 	{
 
-		System.out.println(getBooksListDistinct());
-		System.out.println(getBooksPriceSum());
-		System.out.println(getBooksPriceAvg());
-		System.out.println(getBooksCount());
-		System.out.println(getBooksProjections());
-		System.out.println(getBookNames());
-		System.out.println(getBooksQueryByExample());
-		System.out.println(getBooksQueryByExampleWithLike());
-		System.out.println(getBooksListEqual());
-		System.out.println(getBooksListNotEqual());
+		AuthorOneToManySet.saveAuthor( AuthorOneToManySet.setRouling());
+
 	}
 }
